d854f8bcd5d626d0790fbd8d7b451fc5c7edbce1
mohan.v <mohan.v@conceptglobal.com>
2024-12-12 17:54:28 +0530

    added mono repo structure

--- apps/computerix/src/computerix.module.ts
+++ apps/computerix/src/computerix.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ComputerixController } from './computerix.controller';
 import { ComputerixService } from './computerix.service';
+import { ConfigModule } from '@nestjs/config';
+import path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    // console.log('Current working directory:', process.cwd());
+
+    // ConfigModule.forRoot({
+    //   isGlobal: true,  // Makes the config globally accessible
+    //   envFilePath: [path.resolve(__dirname, '..', '..', '.env')],
+    // }),
+  ],
   controllers: [ComputerixController],
   providers: [ComputerixService],
 })
